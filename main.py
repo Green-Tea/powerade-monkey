2de1e87083d8ee5bb92ccfc93f2b46a403519028
Green-Tea <sittingducks.lorenzo@gmail.com>
2024-03-18 19:03:48 +0700

    added shutdown function

--- main.py
+++ main.py
@@ -3,9 +3,7 @@ from typing import Final
 import os
 from dotenv import load_dotenv
 from discord import Intents, Client, Message
-from responses import get_response
 from datetime import datetime, timedelta, timezone
-from asyncio import Lock
 
 load_dotenv()
 TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
@@ -18,21 +16,36 @@ client: Client = Client(intents=intents)
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# Create a lock for message processing
-message_lock = Lock()
-
 async def send_message(message: Message, user_message: str) -> None:
     if not user_message:
         logger.warning('Message was empty because intents were not enabled properly')
         return
     
+    if user_message.startswith('?'):
+        user_message = user_message[1:]
+        is_private = True
+    else:
+        is_private = False
+
     try:
         response: str = get_response(user_message)
-        await message.channel.send(response)
+        if is_private:
+            await message.author.send(response)
+        else:
+            await message.channel.send(response)
     except Exception as e:
         logger.error(f'Error: {e}')
 
-async def shutdown_bot() -> None:
+def get_response(user_input: str) -> str:
+    lowered: str = user_input.lower()
+    
+    if lowered == 'gm':
+        return 'https://youtube.com/shorts/r2lpAnZ1hLQ?si=f5Im2rEi7Hvn7nSr'
+    
+    if lowered == 'shutdown':
+        return 'Shutting down'
+
+async def shutdown() -> None:
     logger.info('Shutting down the bot...')
     await client.close()
 
@@ -48,23 +61,20 @@ async def on_message(message: Message) -> None:
     username: str = message.author.name
     user_message: str = message.content
     channel: str = message.channel.name
-
-    if username == '_seaweed' and (channel == 'general' or channel == 'cunt-convos'):
-        async with message_lock:
-            now_utc = datetime.now(timezone.utc)
-            utc_plus_7 = now_utc + timedelta(hours=7)
-            timestamp = utc_plus_7.strftime('%Y-%m-%d %H:%M:%S')
-            logger.info(f'Time of awakening: {timestamp}')
-            await send_message(message, user_message)
-
-    # Check if the message is a DM from you and the content is 'shutdown'
-    if isinstance(message.channel, discord.DMChannel) and message.author.id == '_seaweed' and message.content == 'shutdown':
-        await shutdown_bot()
+    
+    if username == '_seaweed' and (channel == 'general' or channel == 'cunt-convos') and user_message.lower() == 'gm':
+        await send_message(message, user_message)
+        now_utc = datetime.now(timezone.utc)
+        utc_plus_7 = now_utc + timedelta(hours=7)
+        timestamp = utc_plus_7.strftime('%Y-%m-%d %H:%M:%S')
+        logger.info(f'Time of awakening: {timestamp}')
+    elif username == '_seaweed' and channel == 'shutdown':
+        await send_message(message, user_message='shutdown')
+        await shutdown()
 
     return
 
 def main() -> None:
-    client.close() # prevent multiple instances of client
     client.run(token=TOKEN)
 
 if __name__ == '__main__':
--- responses.py
+++
@@ -1,5 +0,0 @@
-def get_response(user_input: str) -> str:
-    lowered: str = user_input.lower()
-    
-    if lowered == 'gm':
-        return 'https://youtube.com/shorts/r2lpAnZ1hLQ?si=f5Im2rEi7Hvn7nSr'
