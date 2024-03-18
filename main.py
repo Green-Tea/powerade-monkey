7c10f982fbd2858232877cb03e72211941331994
Green-Tea <sittingducks.lorenzo@gmail.com>
2024-03-18 17:26:08 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,50 @@
+from typing import Final
+import os
+from dotenv import load_dotenv
+from discord import Intents, Client, Message
+from responses import get_response
+
+load_dotenv()
+TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
+
+
+intents: Intents = Intents.default()
+intents.message_content = True # NOQA
+client: Client = Client(intents=intents)
+
+async def send_message(message: Message, user_message: str) -> None:
+    if not user_message:
+        print('Message was empty because intents were not enabled properly')
+        return
+    
+    try:
+        response: str = get_response(user_message)
+        await message.channel.send(response)
+    except Exception as e:
+        print(f'Error: {e}')
+        
+@client.event
+async def on_ready() -> None:
+    print(f'{client.user} has connected to Discord!')
+
+@client.event
+async def on_message(message: Message) -> None:
+    if message.author == client.user:
+        return
+    
+    username: str = message.author.name
+    user_message: str = message.content
+    channel: str = message.channel.name
+    
+    if username != '_seaweed':
+        return
+    
+    print(f'{username} said {user_message} in {channel}')
+
+    await send_message(message, user_message)
+
+def main() -> None:
+    client.run(token=TOKEN)
+
+if __name__ == '__main__':
+    main()
---
+++ responses.py
@@ -0,0 +1,5 @@
+def get_response(user_input: str) -> str:
+    lowered: str = user_input.lower()
+    
+    if lowered == 'gm':
+        return 'https://youtube.com/shorts/r2lpAnZ1hLQ?si=f5Im2rEi7Hvn7nSr'
