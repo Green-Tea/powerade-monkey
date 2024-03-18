f5489a667b618c13812a5e0f0d56ff07125a562f
Green-Tea <sittingducks.lorenzo@gmail.com>
2024-03-18 17:58:35 +0700

    replace print with logger

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import logging
 from typing import Final
 import os
 from dotenv import load_dotenv
@@ -12,20 +13,24 @@ intents: Intents = Intents.default()
 intents.message_content = True
 client: Client = Client(intents=intents)
 
+# Configure logger
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 async def send_message(message: Message, user_message: str) -> None:
     if not user_message:
-        print('Message was empty because intents were not enabled properly')
+        logger.warning('Message was empty because intents were not enabled properly')
         return
     
     try:
         response: str = get_response(user_message)
         await message.channel.send(response)
     except Exception as e:
-        print(f'Error: {e}')
+        logger.error(f'Error: {e}')
         
 @client.event
 async def on_ready() -> None:
-    print(f'{client.user} has connected to Discord!')
+    logger.info(f'{client.user} has connected to Discord!')
 
 @client.event
 async def on_message(message: Message) -> None:
@@ -37,10 +42,10 @@ async def on_message(message: Message) -> None:
     channel: str = message.channel.name
 
     if username == '_seaweed' and (channel == 'general' or channel == 'cunt-convos'):
-        now_utc: datetime = datetime.now(timezone.utc)
-        utc_plus_7: timedelta = now_utc + timedelta(hours=7)
-        timestamp: str = utc_plus_7.strftime('%Y-%m-%d %H:%M:%S')
-        print(f'Time of awakening: {timestamp}')
+        now_utc = datetime.now(timezone.utc)
+        utc_plus_7 = now_utc + timedelta(hours=7)
+        timestamp = utc_plus_7.strftime('%Y-%m-%d %H:%M:%S')
+        logger.info(f'Time of awakening: {timestamp}')
         await send_message(message, user_message)
 
     return
